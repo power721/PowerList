de461fe482e5894dd5797cff155666b64aca725a
Har01d <power0721@gmail.com>
2026-05-31 16:42:14 +0800

    迅雷云盘同步token

--- drivers/thunder_browser/driver.go
+++ drivers/thunder_browser/driver.go
@@ -79,6 +79,7 @@ func (x *ThunderBrowser) Init(ctx context.Context) (err error) {
 				ID:                x.ID,
 				refreshCTokenCk: func(_token string) {
 					x.CaptchaToken = _token
+					x.SetTokenResp(x.TokenResp)
 					op.MustSaveDriverStorage(x)
 				},
 			},
@@ -220,6 +221,7 @@ func (x *ThunderBrowserExpert) Init(ctx context.Context) (err error) {
 				ID:            x.ID,
 				refreshCTokenCk: func(_token string) {
 					x.CaptchaToken = _token
+					x.SetTokenResp(x.TokenResp)
 					op.MustSaveDriverStorage(x)
 				},
 			},
