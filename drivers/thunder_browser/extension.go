e4c940eff60908ecdf0a267c83c1fc6b1f4891c2
Har01d <power0721@gmail.com>
2026-06-07 13:03:14 +0800

    修复迅雷

--- drivers/thunder_browser/extension.go
+++ drivers/thunder_browser/extension.go
@@ -2,35 +2,57 @@ package thunder_browser
 
 import (
 	"context"
+	"errors"
+	"strings"
+	"time"
 
 	"github.com/OpenListTeam/OpenList/v4/internal/conf"
 	log "github.com/sirupsen/logrus"
 )
 
 func (y *ThunderBrowser) createTempDir(ctx context.Context) error {
-	dir := &Files{
-		ID:    "",
-		Space: "",
-	}
-	err := y.MakeDir(ctx, dir, conf.TempDirName)
-	if err != nil {
-		log.Warnf("create Thunder temp dir failed: %v", err)
-	}
+	transferDir := ""
+	sleep := time.Second * 2
+	time.Sleep(sleep)
 
-	files, err := y.getFiles(ctx, dir, "")
-	if err != nil {
-		return err
-	}
-
-	for _, file := range files {
-		if file.GetName() == conf.TempDirName {
-			y.TempDirId = file.GetID()
-			break
+	for range 10 {
+		dir := &Files{
+			ID:    "",
+			Space: "",
 		}
-	}
+		err := y.MakeDir(ctx, dir, conf.TempDirName)
+		if err != nil {
+			log.Warnf("create Thunder temp dir failed: %v", err)
+			if strings.Contains(err.Error(), "captcha_invalid") {
+				time.Sleep(sleep)
+				continue
+			} else {
+				return err
+			}
+		}
 
-	log.Info("Thunder temp folder id: ", y.TempDirId)
-	return nil
+		files, err := y.getFiles(ctx, dir, "")
+		if err != nil {
+			log.Warnf("Thunder list files failed: %v", err)
+			return err
+		}
+
+		for _, file := range files {
+			if file.GetName() == "我的转存" {
+				transferDir = file.GetID()
+			}
+			if file.GetName() == conf.TempDirName {
+				y.TempDirId = file.GetID()
+				break
+			}
+		}
+
+		log.Info("Thunder temp folder id: ", y.TempDirId)
+		return nil
+	}
+	y.TempDirId = transferDir
+	log.Info("Thunder transfer folder id: ", y.TempDirId)
+	return errors.New("Thunder temp folder id not found")
 }
 
 func (y *ThunderBrowser) createOfflineDir(ctx context.Context) error {
